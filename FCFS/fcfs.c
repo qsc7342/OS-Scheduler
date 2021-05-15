6540e3b87eb1a999290cf5183b09b3ce0877895e
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-05-15 22:13:59 +0900

    fcfs 임시 코드

--- FCFS/fcfs.c
+++ FCFS/fcfs.c
@@ -5,7 +5,6 @@
 
 #define PROCESS 10
 
-
 /*
     프로세스 구조체
 */
@@ -17,10 +16,21 @@ typedef struct process {
 }process;
 
 int cnt = 0;
+int visit[100][PROCESS] = {}; // 간트차트 생성용
 
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+int max(int a,int b) {
+    return a > b ? a : b;
+}
 
 void makeline() {
-    printf("==============================\n");
+    for(int i = 0 ; i < 120 ; i++) {
+        printf("=");
+    }
+    printf("\n");
 }
 
 /*
@@ -64,27 +74,84 @@ int main() {
     FILE* fi;
     process proc[PROCESS];
 
+    // 파일에서 프로세스 정보 읽어오는 과정
     fi = fopen("process.txt", "r");
-    int i = 0;
-    for(i = 0 ; !feof(fi) ; i++) {
+    for(int i = 0 ; !feof(fi) ; i++) {
         ++cnt;
         fscanf(fi, "%d %d %d\n", &proc[i].arrive_time, &proc[i].burst_time, &proc[i].deadline);
         char *pnum;
-        sprintf(pnum, "Process[%d]", i + 1);
+        pnum = malloc(sizeof(char) * 10);
+        sprintf(pnum, "Process %d", i + 1);
         proc[i].pname = malloc(sizeof(char) * 10);
         strcpy(proc[i].pname, pnum);
+    } 
+
+    // 정렬
+    sort(proc);
+    
+    int tot_wait = 0; // 총 대기시간, 이후에 평균 대기시간 구할 때 사용
+    int cur_time = 0; // CPU 시간. 대기 시간 등을 구할 때 사용
+    int wait_time[PROCESS] = {}; // 각 프로세스 별 대기 시간, 필요는 있을지 모르겠음.
+
+    for(int i = 0 ; i < cnt ; i++) {
+        int cur_arrive = proc[i].arrive_time;
+        int cur_burst = proc[i].burst_time;
+        int cur_dead = proc[i].deadline;
+        char *pnum;
+        // 프로세스 이름에서 프로세스 넘버를 추출하기 위함. (정렬로 인한 필요)
+        pnum = strtok(proc[i].pname, " ");
+        pnum = strtok(NULL, " ");
+        int real_pnum = atoi(pnum); // 문자열 -> 정수형 변환
+        wait_time[real_pnum] = max(0, cur_time - cur_arrive); // 대기시간 : 현재 시간 - 도착 시간과 0 중 최솟값
+        tot_wait += wait_time[real_pnum]; // 토탈에 더함
+        for(int j = cur_arrive ; j < cur_time ; j++) {
+            if(i == 0) printf("wait %d\n", j);
+            visit[j][real_pnum] = -1;
+        } // 도착 ~ 현재시간까지는 대기시간이므로 -1
+
+        for(int j = cur_time ; j < cur_time + cur_burst ; j++) {
+            if(i == 0) printf("execute %d\n", j);
+            visit[j][real_pnum] = 1;
+        }  // 현재시각 ~ 현재시각 + 실행시간은 실행시간이므로 1
+        cur_time += cur_burst;
     }
 
-    for(int j = 0 ; j < i ; j++) {
-        printf("%s : %d %d %d\n", proc[j].pname, proc[j].arrive_time, proc[j].burst_time, proc[j].deadline);
+
+    // 간트 차트 생성
+    makeline();
+    printf("            X : not arrive,  W : waiting,  O : executing\n            Time\n");
+    printf("            ");
+    int chk = 0;
+    for(int k = 0 ; k <= 100 ; k++) {
+        if(k % 10 == 0) printf("%d", chk++);
+        else printf(" ");
     }
-    sort(proc);
+    printf("\n");
+
+    printf("            ");
+    for(int k = 0 ; k <= 100 ; k++) {
+        printf("%d", k%10);
+    }
+    printf("\n");
+    for(int i = 0 ; i < cnt ; i++) {
+        printf("PROCESS %d : ", i + 1);
+        for(int j = 0 ; j <= 100 ; j++) {
+            if(visit[j][i + 1] == -1) {
+                printf("W");
+            }
+            else if(visit[j][i + 1] == 1) {
+                printf("O");
+            }
+            else if(visit[j][i + 1] == 0) {
+                printf("X");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Average wait time : %.2lf\n",(double)tot_wait/cnt);
     makeline();
-    printf("After Sorting...\n");
-    makeline();
-    for(int j = 0 ; j < i ; j++) {
-        printf("%s : %d %d %d\n", proc[j].pname, proc[j].arrive_time, proc[j].burst_time, proc[j].deadline);
-    }  
+    
     fclose(fi);
     return 0;
 }
